d1f8eeaaa18e9427ac875acd04771aa7e15b5c16
VikrantReddy <vikrantreddy@hotmail.com>
2020-10-21 22:04:05 +0530

    Code refactoring

--- getCoordinates.py
+++ getCoordinates.py
@@ -1,14 +1,15 @@
 import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
+import os
 
-api_key = False
-# If you have a Google Places API key, enter it here
-# api_key = 'AIzaSy___IDByT70'
+API_KEY = os.environ.get("API_KEY",False)
+
+# If you dont have a Google Places API key, find it here
 # https://developers.google.com/maps/documentation/geocoding/intro
 
-if api_key is False:
-    api_key = 42
+if API_KEY is False:
+    API_KEY = 42
     serviceurl = 'http://py4e-data.dr-chuck.net/json?'
 else :
     serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
@@ -18,34 +19,53 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-while True:
-    address = input('Enter location: ')
-    if len(address) < 1: break
 
-    parms = dict()
-    parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
+def getData(address):
+    if len(address) < 1: 
+        return
+
+    parms = {}
+    parms.update({'address' : address})
+    
+    if API_KEY is not False: 
+        parms['key'] = API_KEY
+
     url = serviceurl + urllib.parse.urlencode(parms)
 
     print('Retrieving', url)
-    uh = urllib.request.urlopen(url, context=ctx)
-    data = uh.read().decode()
-    print('Retrieved', len(data), 'characters')
+    data = urllib.request.urlopen(url, context=ctx)
+    decodeddata = data.read().decode()
 
+    return decodeddata
+
+
+def getCordinates(data):
     try:
         js = json.loads(data)
-    except:
+    except TypeError:
         js = None
 
     if not js or 'status' not in js or js['status'] != 'OK':
-        print('==== Failure To Retrieve ====')
-        print(data)
-        continue
+        return -1
 
-    print(json.dumps(js, indent=4))
+    latitude = js['results'][0]['geometry']['location']['lat']
+    longitude = js['results'][0]['geometry']['location']['lng']
 
-    lat = js['results'][0]['geometry']['location']['lat']
-    lng = js['results'][0]['geometry']['location']['lng']
-    print('lat', lat, 'lng', lng)
     location = js['results'][0]['formatted_address']
-    print(location)
+    
+    return latitude,longitude,location
+
+    
+if __name__ == "__main__" : 
+    while True:
+        address = input('Enter location: ')
+        data = getData(address=address)
+
+        print('Retrieved', len(data), 'characters')
+
+        latitude,longitude,location = getCordinates(data=data)
+
+        print(f"latitude:{latitude}, longitude= {longitude}")
+        print(f"Loc: {location}")
+
+    
